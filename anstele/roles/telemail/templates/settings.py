d48ab491fb5ed99bafcf238f3469a146725e0a80
Jiri Barton <jbar@hosting4u.cz>
2014-12-22 12:21:37 +0100

    telemail password

--- anstele/roles/telemail/templates/settings.py
+++ anstele/roles/telemail/templates/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_stackato',
     'telemail.domains',
     'telemail.addresses',
     'telemail.spam',
