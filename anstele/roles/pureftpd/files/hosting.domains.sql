188c05c9875f4f1db49170ac19f26cd81b60f641
Jiri Barton <jbar@hosting4u.cz>
2015-01-22 15:48:00 +0100

    FTP password length

--- anstele/roles/pureftpd/files/hosting.domains.sql
+++ anstele/roles/pureftpd/files/hosting.domains.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS domains;
 CREATE TABLE domains (
   userid varchar(100) NOT NULL DEFAULT '',
-  passwd varchar(20) DEFAULT NULL,
+  passwd varchar(128) DEFAULT NULL,
   quota int(11) NOT NULL DEFAULT '0',
   homedir varchar(100) NOT NULL DEFAULT '',
   PRIMARY KEY (userid)
